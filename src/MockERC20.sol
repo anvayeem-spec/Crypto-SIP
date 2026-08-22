69cd700501948c5b9e59b5f25bbb0f406001b2d8
anvayeem-spec <anvayeem18@gmail.com>
2026-08-22 22:48:33 +0530

    feat(MockERC20): add totalSupply, zero-address checks, and allowance helpers

--- src/MockERC20.sol
+++ src/MockERC20.sol
@@ -7,6 +7,8 @@ contract MockERC20 {
     string public symbol;
     uint8 public decimals = 18;
 
+    uint256 public totalSupply;
+
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
@@ -18,7 +20,10 @@ contract MockERC20 {
         symbol = _symbol;
     }
 
+    /// @notice Mint tokens to `to`. Increases totalSupply. `to` cannot be the zero address.
     function mint(address to, uint256 amount) external {
+        require(to != address(0), "mint to the zero address");
+        totalSupply += amount;
         balanceOf[to] += amount;
         emit Transfer(address(0), to, amount);
     }
@@ -26,6 +31,24 @@ contract MockERC20 {
     function approve(address spender, uint256 amount) external returns (bool) {
         allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    /// @notice Increase approval for `spender` by `addedValue`.
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool) {
+        allowance[msg.sender][spender] += addedValue;
+        emit Approval(msg.sender, spender, allowance[msg.sender][spender]);
+        return true;
+    }
+
+    /// @notice Decrease approval for `spender` by `subtractedValue`.
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
+        uint256 current = allowance[msg.sender][spender];
+        require(current >= subtractedValue, "decreased allowance below zero");
+        unchecked {
+            allowance[msg.sender][spender] = current - subtractedValue;
+        }
+        emit Approval(msg.sender, spender, allowance[msg.sender][spender]);
         return true;
     }
 
@@ -39,15 +62,20 @@ contract MockERC20 {
         require(allowed >= amount, "insufficient allowance");
         if (allowed != type(uint256).max) {
             allowance[from][msg.sender] = allowed - amount;
+            emit Approval(from, msg.sender, allowance[from][msg.sender]);
         }
         _transfer(from, to, amount);
         return true;
     }
 
     function _transfer(address from, address to, uint256 amount) internal {
+        require(from != address(0), "transfer from the zero address");
+        require(to != address(0), "transfer to the zero address");
         require(balanceOf[from] >= amount, "insufficient balance");
-        balanceOf[from] -= amount;
-        balanceOf[to] += amount;
+        unchecked {
+            balanceOf[from] -= amount;
+            balanceOf[to] += amount;
+        }
         emit Transfer(from, to, amount);
     }
 }
